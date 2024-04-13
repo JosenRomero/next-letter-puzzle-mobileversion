42095d12e30184b1e247223835e21c7b315459f6
Jose <romerojosemanuel@hotmail.com>
2024-04-13 17:41:58 -0600

    feat: add splashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-rc01")
+    implementation("androidx.compose.material3:material3:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -70,6 +70,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("com.google.code.gson:gson:2.9.0")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     // navigation-compose
     val navVersion = "2.7.6"
--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/main/MainActivity.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.josenromero.nextletterpuzzle.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.josenromero.nextletterpuzzle.ui.main.navigation.AppNavigation
 import com.josenromero.nextletterpuzzle.ui.theme.NextLetterPuzzleTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             NextLetterPuzzleTheme {
                 AppNavigation()
