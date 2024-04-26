fdbea4bf9efd621eb61ed89f240da82de38c3447
Jose <romerojosemanuel@hotmail.com>
2024-04-26 17:30:50 -0600

    add SimpleCard Component

---
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/SimpleCard.kt
@@ -0,0 +1,38 @@
+package com.josenromero.nextletterpuzzle.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SimpleCard(
+    defaultElevation: Dp = 0.dp,
+    content: @Composable (() -> Unit)
+) {
+
+    Card(
+        Modifier.padding(vertical = 10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = defaultElevation
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            content()
+        }
+
+    }
+
+}
