11f1b050d2899d4ee05b3965270dfc30bff24ac0
Jose <romerojosemanuel@hotmail.com>
2024-05-04 18:11:11 -0600

    feat: add a RoundedCornerShape in CustomBottomAppBar component

--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/CustomBottomAppBar.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,7 +17,11 @@ fun CustomBottomAppBar(
 ) {
 
     BottomAppBar(
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .clip(
+                shape = RoundedCornerShape(50.dp)
+            )
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
