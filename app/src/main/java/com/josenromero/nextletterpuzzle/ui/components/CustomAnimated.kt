62d6bd23049da27b7a78ed233dbc35911f587646
Jose <romerojosemanuel@hotmail.com>
2024-05-06 19:15:39 -0600

    refactor: CustomAnimated component

--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/CustomAnimated.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/CustomAnimated.kt
@@ -2,6 +2,8 @@ package com.josenromero.nextletterpuzzle.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
@@ -10,16 +12,45 @@ import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import kotlinx.coroutines.launch
 
 @Composable
-fun AnimatedFadeAndExpand(
-    visible: Boolean = true,
+fun CustomAnimated(
+    enter: EnterTransition,
+    exit: ExitTransition,
     content:  @Composable AnimatedVisibilityScope.() -> Unit
 ) {
 
+    var visible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = Unit) {
+        launch {
+            visible = true
+        }
+    }
+
     AnimatedVisibility(
         visible = visible,
-        enter = fadeIn() + expandIn(),
+        enter = enter,
+        exit = exit,
+        content = content
+    )
+
+}
+
+@Composable
+fun AnimatedFadeAndExpand(
+    content:  @Composable AnimatedVisibilityScope.() -> Unit
+) {
+
+    CustomAnimated(
+        enter = fadeIn() + expandIn(expandFrom = Alignment.Center),
         exit = shrinkOut() + fadeOut(),
         content = content
     )
@@ -28,14 +59,12 @@ fun AnimatedFadeAndExpand(
 
 @Composable
 fun AnimatedSlideVertically(
-    visible: Boolean = true,
     content:  @Composable AnimatedVisibilityScope.() -> Unit
 ) {
 
-    AnimatedVisibility(
-        visible = visible,
-        enter = slideInVertically(tween(durationMillis = 300)),
-        exit = slideOutVertically(tween(durationMillis = 300)),
+    CustomAnimated(
+        enter = slideInVertically(tween(durationMillis = 700)),
+        exit = slideOutVertically(tween(durationMillis = 700)),
         content = content
     )
 
