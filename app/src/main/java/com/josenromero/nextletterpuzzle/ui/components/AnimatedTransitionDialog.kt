3d2dd076a90ecdbfd9f523c73758b27ef2d97e1d
Jose <romerojosemanuel@hotmail.com>
2024-09-10 16:39:01 -0600

    update AnimatedTransitionDialog

--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/AnimatedTransitionDialog.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/AnimatedTransitionDialog.kt
@@ -6,19 +6,16 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.josenromero.nextletterpuzzle.ui.theme.NextLetterPuzzleTheme
 import kotlinx.coroutines.delay
@@ -60,17 +57,12 @@ fun AnimatedTransitionDialog(
 
     Dialog(onDismissRequest = onDismissRequest) {
         AnimatedScaleInTransition(visible = animateTrigger.value) {
-            Box {
-                Spacer(modifier = Modifier.height(130.dp))
-                Box(
-                    modifier = Modifier
-                        .background(
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            shape = RoundedCornerShape(25.dp, 10.dp, 25.dp, 10.dp)
-                        )
-                ) {
-                    content()
-                }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                content()
             }
         }
     }
