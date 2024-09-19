b476dc6c9260b2f1f06a908ad4d7ca55952e060e
Jose <romerojosemanuel@hotmail.com>
2024-09-19 15:26:13 -0600

    add a Card component in AchievementUnlocked

--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/AchievementUnlocked.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/AchievementUnlocked.kt
@@ -9,11 +9,10 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,7 +20,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -67,18 +65,20 @@ fun AchievementUnlocked(
         ) + fadeIn(initialAlpha = 0.3f),
         exit = slideOutVertically() + shrinkVertically() + fadeOut() + scaleOut(targetScale = 1.2f)
     ) {
-        Column(
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
-                .background(
-                    color = Color.White,
-                    shape = RoundedCornerShape(10.dp)
-                )
+                .padding(horizontal = 8.dp, vertical = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.onPrimary
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            )
         ) {
             SimpleText(
                 text = text,
-                modifier = Modifier.padding(20.dp),
+                modifier = Modifier.padding(16.dp),
                 color = MaterialTheme.colorScheme.primary,
                 fontWeight = FontWeight.SemiBold,
                 textAlign = TextAlign.Center
