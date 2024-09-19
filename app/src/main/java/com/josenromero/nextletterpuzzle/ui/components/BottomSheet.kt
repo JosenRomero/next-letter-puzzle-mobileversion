4a1a3a39017a2a45340c27192323bf580b68e290
Jose <romerojosemanuel@hotmail.com>
2024-09-19 15:26:45 -0600

    add vertical scroll in BottomSheet

--- app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/BottomSheet.kt
+++ app/src/main/java/com/josenromero/nextletterpuzzle/ui/components/BottomSheet.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,7 +41,8 @@ fun BottomSheet(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             SimpleText(
